103226e8922816bab7a952ba656aac6ca17db8b1
yuzi <hayuzi@163.com>
2019-01-13 20:47:05 +0800

    init

--- models/models.go
+++ models/models.go
@@ -20,8 +20,8 @@ type Model struct {
 
 func init() {
 	var (
-		err                                               error
-		dbType, dbName, user, password, host, tablePrefix string
+		err                                                        error
+		dbType, dbName, user, password, host, charset, tablePrefix string
 	)
 
 	sec, err := setting.Cfg.GetSection("database")
@@ -29,18 +29,20 @@ func init() {
 		log.Fatal(2, "Fail to get section 'database': %v", err)
 	}
 
-	dbType = sec.Key("TYPE").String()
-	dbName = sec.Key("NAME").String()
-	user = sec.Key("USER").String()
-	password = sec.Key("PASSWORD").String()
-	host = sec.Key("HOST").String()
+	dbType = sec.Key("DB_TYPE").String()
+	dbName = sec.Key("DB_NAME").String()
+	user = sec.Key("DB_USER").String()
+	password = sec.Key("DB_PASSWORD").String()
+	host = sec.Key("DB_HOST").String()
+	charset = sec.Key("DB_CHARSET").String()
 	tablePrefix = sec.Key("TABLE_PREFIX").String()
 
-	db, err = gorm.Open(dbType, fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+	db, err = gorm.Open(dbType, fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=%s&parseTime=True&loc=Local",
 		user,
 		password,
 		host,
-		dbName))
+		dbName,
+		charset))
 
 	if err != nil {
 		log.Println(err)
