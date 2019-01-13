4cea972916617472a71fccdedd71661792a951c5
yuzi <hayuzi@163.com>
2019-01-13 22:30:02 +0800

    init

--- main.go
+++ main.go
@@ -2,18 +2,15 @@ package main
 
 import (
 	"fmt"
-	"github.com/gin-gonic/gin"
 	"net/http"
+
 	"blog-go-server/pkg/setting"
+	"blog-go-server/routers"
 )
 
 func main() {
-	router := gin.Default()
-	router.GET("/test", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "test",
-		})
-	})
+
+	router := routers.InitRouter()
 
 	s := &http.Server{
 		Addr:           fmt.Sprintf(":%d", setting.HTTPPort),
