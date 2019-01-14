7e57a7cdde5918fe390374f64872743a2805334c
yuzi <hayuzi@163.com>
2019-01-14 22:07:59 +0800

    init

--- routers/router.go
+++ routers/router.go
@@ -3,6 +3,7 @@ package routers
 import (
 	"github.com/gin-gonic/gin"
 	"blog-go-server/pkg/setting"
+	"blog-go-server/routers/api/v1"
 )
 
 func InitRouter() *gin.Engine {
@@ -14,11 +15,17 @@ func InitRouter() *gin.Engine {
 
 	gin.SetMode(setting.RunMode)
 
-	r.GET("/test", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "test",
-		})
-	})
+	apiV1 := r.Group("/api/v1")
+	{
+		//获取标签列表
+		apiV1.GET("/tags", v1.GetTags)
+		//新建标签
+		apiV1.POST("/tags", v1.AddTag)
+		//更新指定标签
+		apiV1.PUT("/tags/:id", v1.EditTag)
+		//删除指定标签
+		apiV1.DELETE("/tags/:id", v1.DeleteTag)
+	}
 
 	return r
 }
