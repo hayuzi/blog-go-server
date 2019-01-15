c540657507c78982d02c9c412ef5745e66a2b7a9
hayuzi <hayuzi@163.com>
2019-01-16 00:08:44 +0800

    init

--- pkg/constmap/commonconst.go
+++ pkg/constmap/commonconst.go
@@ -1,4 +1,10 @@
 package constmap
 
+// 伪删除
 const DEL_STATUS_NORMAL = 1  // 未删除
 const DEL_STATUS_DELETED = 2 // 已经删除
+
+// 分页相关
+const DEFAULT_PAGE_NUM = 1    // 默认页码
+const DEFAULT_PAGE_LIMIT = 10 // 默认每页条目数
+const MAX_PAGE_LIMIT = 100    // 每页条目数最大限制
--- pkg/util/pagination.go
+++ pkg/util/pagination.go
@@ -1,17 +1,34 @@
 package util
 
 import (
-	"blog-go-server/pkg/setting"
+	"blog-go-server/pkg/constmap"
 	"github.com/Unknwon/com"
 	"github.com/gin-gonic/gin"
 )
 
-func GetPage(c *gin.Context) int {
+// 根据页码与每页数量获取sql的查询偏移量
+func GetQueryOffset(pageNum int, pageLimit int) int {
 	result := 0
-	page, _ := com.StrTo(c.Query("page")).Int()
-	if page > 0 {
-		result = (page - 1) * setting.PageSize
+	if pageNum > 0 {
+		result = (pageNum - 1) * pageLimit
 	}
+	return result
+}
 
+func GetPageNum(c *gin.Context) int {
+	result := 1
+	pageNum, _ := com.StrTo(c.Query("page_num")).Int()
+	if pageNum >= 1 {
+		return pageNum
+	}
+	return result
+}
+
+func GetPageLimit(c *gin.Context) int {
+	result := constmap.DEFAULT_PAGE_LIMIT
+	pageLimit, _ := com.StrTo(c.Query("page_limit")).Int()
+	if pageLimit > 0 && pageLimit <= constmap.MAX_PAGE_LIMIT {
+		result = pageLimit
+	}
 	return result
 }
