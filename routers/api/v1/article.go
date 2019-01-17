ee9a2156eab6f6d83357fcf31144dc95be23495d
hayuzi <hayuzi@163.com>
2019-01-17 23:08:28 +0800

    init

---
+++ routers/api/v1/article.go
@@ -0,0 +1,25 @@
+package v1
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+//获取单个文章
+func GetArticle(c *gin.Context) {
+}
+
+//获取多个文章
+func GetArticles(c *gin.Context) {
+}
+
+//新增文章
+func AddArticle(c *gin.Context) {
+}
+
+//修改文章
+func EditArticle(c *gin.Context) {
+}
+
+//删除文章
+func DeleteArticle(c *gin.Context) {
+}
