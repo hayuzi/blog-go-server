9d2b414795f4ba07b8ecde11937e8f387408da95
hayuzi <hayuzi@163.com>
2019-01-29 23:36:36 +0800

    init

---
+++ pkg/upload/image.go
@@ -0,0 +1,77 @@
+package upload
+
+import (
+	"blog-go-server/pkg/file"
+	"blog-go-server/pkg/logging"
+	"blog-go-server/pkg/setting"
+	"blog-go-server/pkg/util"
+
+	"fmt"
+	"log"
+	"mime/multipart"
+	"os"
+	"path"
+	"strings"
+)
+
+func GetImageFullUrl(name string) string {
+	// TODO imageHost
+	return setting.AppSetting.PrefixUrl + "/" + GetImagePath() + name
+}
+
+func GetImageName(name string) string {
+	ext := path.Ext(name)
+	fileName := strings.TrimSuffix(name, ext)
+	fileName = util.EncodeMD5(fileName)
+
+	return fileName + ext
+}
+
+func GetImagePath() string {
+	return setting.AppSetting.ImageSavePath
+}
+
+func GetImageFullPath() string {
+	return setting.AppSetting.RuntimeRootPath + GetImagePath()
+}
+
+func CheckImageExt(fileName string) bool {
+	ext := file.GetExt(fileName)
+	for _, allowExt := range setting.AppSetting.ImageAllowExts {
+		if strings.ToUpper(allowExt) == strings.ToUpper(ext) {
+			return true
+		}
+	}
+
+	return false
+}
+
+func CheckImageSize(f multipart.File) bool {
+	size, err := file.GetSize(f)
+	if err != nil {
+		log.Println(err)
+		logging.Warn(err)
+		return false
+	}
+
+	return size <= setting.AppSetting.ImageMaxSize
+}
+
+func CheckImage(src string) error {
+	dir, err := os.Getwd()
+	if err != nil {
+		return fmt.Errorf("os.Getwd err: %v", err)
+	}
+
+	err = file.IsNotExistMkDir(dir + "/" + src)
+	if err != nil {
+		return fmt.Errorf("file.IsNotExistMkDir err: %v", err)
+	}
+
+	perm := file.CheckPermission(src)
+	if perm == true {
+		return fmt.Errorf("file.CheckPermission Permission denied src: %s", src)
+	}
+
+	return nil
+}
