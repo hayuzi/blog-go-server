09b645ea1d5e92064cef2584dc33fcc4763387fb
yuzi <hayuzi@163.com>
2019-02-15 15:17:38 +0800

    init

--- routers/api/v0/upload.go
+++ routers/api/v0/upload.go
@@ -6,51 +6,48 @@ import (
 	"blog-go-server/pkg/logging"
 	"net/http"
 	"blog-go-server/pkg/upload"
+	"blog-go-server/pkg/app"
 )
 
 func UploadImage(c *gin.Context) {
-	code := e.Success
+	appG := app.Gin{C: c}
 	data := make(map[string]string)
 
 	file, image, err := c.Request.FormFile("image")
 	if err != nil {
 		logging.Warn(err)
-		code = e.Error
-		c.JSON(http.StatusOK, gin.H{
-			"code": code,
-			"msg":  e.GetMsg(code),
-			"data": data,
-		})
+		appG.Response(http.StatusOK, e.Error, data)
+		return
 	}
 
 	if image == nil {
-		code = e.InvalidParams
-	} else {
-		imageName := upload.GetImageName(image.Filename)
-		fullPath := upload.GetImageFullPath()
-		savePath := upload.GetImagePath()
-
-		src := fullPath + imageName
-		if ! upload.CheckImageExt(imageName) || ! upload.CheckImageSize(file) {
-			code = e.ErrorUploadCheckImageFormat
-		} else {
-			err := upload.CheckImage(fullPath)
-			if err != nil {
-				logging.Warn(err)
-				code = e.ErrorUploadCheckImageFail
-			} else if err := c.SaveUploadedFile(image, src); err != nil {
-				logging.Warn(err)
-				code = e.ErrorUploadSaveImageFail
-			} else {
-				data["image_url"] = upload.GetImageFullUrl(imageName)
-				data["image_save_url"] = savePath + imageName
-			}
-		}
+		appG.Response(http.StatusOK, e.InvalidParams, data)
+		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{
-		"code": code,
-		"msg":  e.GetMsg(code),
-		"data": data,
-	})
+	imageName := upload.GetImageName(image.Filename)
+	fullPath := upload.GetImageFullPath()
+	savePath := upload.GetImagePath()
+	src := fullPath + imageName
+
+	if ! upload.CheckImageExt(imageName) || ! upload.CheckImageSize(file) {
+		appG.Response(http.StatusOK, e.ErrorUploadCheckImageFormat, data)
+		return
+	}
+
+	err = upload.CheckImage(fullPath)
+	if err != nil {
+		logging.Warn(err)
+		appG.Response(http.StatusOK, e.ErrorUploadCheckImageFail, data)
+		return
+	} else if err := c.SaveUploadedFile(image, src); err != nil {
+		logging.Warn(err)
+		appG.Response(http.StatusOK, e.ErrorUploadSaveImageFail, data)
+		return
+	} else {
+		data["image_url"] = upload.GetImageFullUrl(imageName)
+		data["image_save_url"] = savePath + imageName
+	}
+
+	appG.Response(http.StatusOK, e.Success, data)
 }
